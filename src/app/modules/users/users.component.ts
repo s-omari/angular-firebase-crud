2e68fbc3db522668fa0b3b3686601d970f9a9c03
saleh omari <s.omari.jor@gmail.com>
2020-04-20 19:04:30 +0400

    writing to firebase

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  itemValue = '';
+  items: Observable<any[]>;
 
-  constructor() { }
+  constructor(public db: AngularFireDatabase) {
+    this.items = db.list('items').valueChanges();
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.db.list('items').push({ content: this.itemValue});
+    this.itemValue = '';
+  }
 }
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import {router , RoutingComponents} from './users-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [UsersComponent, RoutingComponents],
   imports: [
     CommonModule,
-    router
+    router,
+    FormsModule
   ]
 })
 export class UsersModule { }
